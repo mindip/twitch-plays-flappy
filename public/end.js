c2dbf4acb6940dd23c265977488901d84e4844d5
mindip <oacidnim@g.ucla.edu>
2019-03-31 09:16:13 -0700

    scoreboard except it works

--- public/end.js
+++ public/end.js
@@ -40,6 +40,40 @@ endState.prototype = {
       { font: "50px impact", fill: "#ffffff" }
     );
   },
+  preload: function() {
+    let users = {};
+    this.db
+      .collection("scores")
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          user = doc.data()["user"];
+          users[user] = doc.data()["score"];
+        });
+        // Create items array
+        var items = Object.keys(users).map(function(key) {
+          return [key, users[key]];
+        });
+        // Sort the array based on the second element
+        items.sort(function(first, second) {
+          return second[1] - first[1];
+        });
+        items = items.slice(0, 5);
+        scores = "";
+        items.forEach(arr => {
+          scores += arr[1] + "                  " + arr[0] + "\n";
+        });
+        this.scoresText = game.add.text(
+          window.innerWidth / 3,
+          window.innerHeight / 2,
+          scores,
+          { font: "50px impact", fill: "#ffffff" }
+        );
+      })
+      .catch(function(error) {
+        console.log("Error getting SCORES: ", error);
+      });
+  },
   create: function() {
     var enter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     enter.onDown.addOnce(this.getName, this);
